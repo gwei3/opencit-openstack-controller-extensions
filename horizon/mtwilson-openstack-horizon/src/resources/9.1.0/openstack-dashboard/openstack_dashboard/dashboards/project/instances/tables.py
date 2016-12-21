93f76f9bdcd5832d906eec843ac20e0b0e2ccc72
Animesh Patel <animeshx.patel@intel.com>
2016-12-21 13:14:00 +0530

    v3.1-rc1+Bug#6083-Mitaka-Horizon-breaks-after-applying-patches: fixed the issues which break horizon

--- horizon/mtwilson-openstack-horizon/src/resources/9.1.0/openstack-dashboard/openstack_dashboard/dashboards/project/instances/tables.py
+++ horizon/mtwilson-openstack-horizon/src/resources/9.1.0/openstack-dashboard/openstack_dashboard/dashboards/project/instances/tables.py
@@ -791,9 +791,9 @@ class UpdateRow(tables.Row):
         if not hostname is None:
             compute_node = api.nova.hypervisor_search(request, hostname)[0] 
             metadata = api.nova.hvspecs_metadata(request, compute_node)
-	    LOG.error("Fetched metadata")
+            LOG.error("Fetched metadata")
             if hasattr(metadata, 'trust_report'):
-	        LOG.error("trust_report : %s" % metadata.trust_report)
+                LOG.error("trust_report : %s" % metadata.trust_report)
                 instance.attestation_status = metadata.trust_report
             else:
                 instance.attestation_status = None
